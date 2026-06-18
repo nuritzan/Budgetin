cc448a43f891115a0a6161ece14871b6c028ae6c
Muhammad Muthi' Nuritzan <muhammaditzan3@gmail.com>
2026-06-18 14:12:09 +0700

    Add: Add code for Main Card feature at SummaryPage

--- Budgetin/View/SummaryPage/MainCard.swift
+++ Budgetin/View/SummaryPage/MainCard.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct MainCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            VStack(spacing: 4) {
+                Text("Monthly Budget")
+                    .font(Font.title3)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color("PrimaryBlack"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Text("Rp3.500.000")
+                    .font(Font.title3)
+                    .fontWeight(.medium)
+                    .foregroundStyle(Color("PrimaryBlack"))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            
+            ProgressView(value: 596500, total: 3500000)
+                .scaleEffect(y: 2)
+                .tint(Color("PrimaryGreen"))
+            
+            Text("Rp596.500 remaining")
+                .font(Font.title2.bold())
+                .foregroundStyle(Color("PrimaryBlack"))
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+        .padding(20)
+        .frame(width: 360)
+        .glassEffect(in: .rect(cornerRadius: 24))
+        .shadow(radius: 1)
+        .padding(.bottom, 40)
     }
 }
 
