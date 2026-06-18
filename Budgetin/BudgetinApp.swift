833993275d444ac122799b2825ef9a92a0bcfad6
Muhammad Muthi' Nuritzan <muhammaditzan3@gmail.com>
2026-06-18 11:32:14 +0700

    Initial Commit

---
+++ Budgetin/BudgetinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetinApp.swift
+//  Budgetin
+//
+//  Created by Muhammad Muthi' Nuritzan on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct BudgetinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budgetin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budgetin
+//
+//  Created by Muhammad Muthi' Nuritzan on 18/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
