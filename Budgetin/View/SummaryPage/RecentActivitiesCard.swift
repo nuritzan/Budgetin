1e63796796f248f533c4178d26a4bcf9c6893f61
Muhammad Muthi' Nuritzan <muhammaditzan3@gmail.com>
2026-06-18 16:00:46 +0700

    Add: Add code and feature for SummaryPage, the fix the color asset

--- Budgetin/View/SummaryPage/RecentActivitiesCard.swift
+++ Budgetin/View/SummaryPage/RecentActivitiesCard.swift
@@ -7,9 +7,118 @@
 
 import SwiftUI
 
+func getCategoryStyle(category: String) -> (icon: String, iconColor: Color, bgColor: Color) {
+    switch category {
+    case "Meals":
+        return ("fork.knife", Color("Meals"), Color("MealsBackground"))
+    case "Household":
+        return ("house.fill", Color("Household"), Color("HouseholdBackground"))
+    case "Transportation":
+        return ("car.fill", Color("Transport"), Color("TransportBackground"))
+    case "Subscription":
+        return ("creditcard.fill", Color("Subscription"), Color("SubscriptionBackground"))
+    case "Shopping":
+        return ("cart.fill", Color("Shopping"), Color("ShoppingBackground"))
+    case "Entertainment":
+        return ("gamecontroller.fill", Color("Entertainment"), Color("EntertainmentBackground"))
+    case "Health":
+        return ("heart.fill", Color("Health"), Color("HealthBackground"))
+    default:
+        return ("ellipsis.circle.fill", Color("Other"), Color("OtherBackground"))
+    }
+}
+
+@ViewBuilder
+func rowTransactions(category: String, description: String, amount: String) -> some View {
+    let style = getCategoryStyle(category: category)
+    
+    HStack{
+        HStack(spacing: 10) {
+            Image(systemName: style.icon)
+                .font(Font.footnote)
+                .foregroundStyle(style.iconColor)
+                .frame(width: 30, height: 30)
+                .background(style.bgColor)
+                .clipShape(.circle)
+            
+            VStack (alignment: .leading) {
+                Text(category)
+                    .font(Font.callout)
+                    .foregroundStyle(Color("PrimaryBlack"))
+                    .lineLimit(1)
+                
+                Text(description)
+                    .font(Font.caption2)
+                    .foregroundStyle(Color("PrimaryBlack"))
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        
+        Text(amount)
+            .font(Font.callout)
+            .foregroundStyle(Color("PrimaryRed"))
+    }
+}
+
 struct RecentActivitiesCard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Text("Recent Activities")
+                .font(Font.headline)
+                .foregroundStyle(Color("PrimaryBlack"))
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            VStack(spacing: 16) {
+                NavigationLink{
+                    HistoryTransactionsPage()
+                } label: {
+                    HStack{
+                        Text("Transactions")
+                            .font(Font.subheadline)
+                            .foregroundStyle(Color("PrimaryBlack"))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        Image(systemName: "chevron.right")
+                            .font(Font.subheadline)
+                            .foregroundStyle(Color("PrimaryGray"))
+                            .frame(alignment: .trailing)
+                    }
+                }
+                
+                rowTransactions(category: "Subscription", description: "iCloud", amount: "-Rp15.000")
+                rowTransactions(category: "Transportation", description: "Online Taxi", amount: "-Rp18.000")
+                rowTransactions(category: "Transportation", description: "Commuter", amount: "-Rp6.000")
+                rowTransactions(category: "Meals", description: "Dinner", amount: "-Rp32.500")
+            }
+            .padding(16)
+            .frame(maxWidth: .infinity)
+            .glassEffect(in: .rect(cornerRadius: 24))
+            .shadow(radius: 1)
+            
+            NavigationLink{
+                StatisticsPage()
+            } label: {
+                HStack {
+                    Text("Show All Statistics")
+                        .font(Font.callout)
+                        .foregroundStyle(Color("PrimaryBlack"))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Image(systemName: "chevron.right")
+                        .font(Font.callout)
+                        .foregroundStyle(Color("PrimaryGray"))
+                        .frame(alignment: .trailing)
+                }
+                .padding(16)
+                .frame(maxWidth: .infinity)
+                .clipShape(.capsule)
+                .glassEffect()
+                .shadow(radius: 1)
+            }
+            
+            
+        }
+        .frame(width: 360)
     }
 }
 
--- Budgetin/View/SummaryPage/SummaryPage.swift
+++ Budgetin/View/SummaryPage/SummaryPage.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct SummaryPage: View {
+    @State private var isAddTransactionSheetPresented: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("BackgroundColor")
+                .ignoresSafeArea()
+            
+            VStack(spacing: 48) {
+                HStack {
+                    Text("Summary")
+                        .font(Font.largeTitle.bold())
+                        .foregroundStyle(Color("PrimaryBlack"))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    Button{
+                        isAddTransactionSheetPresented = true
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(Font.title2)
+                            .fontWeight(.medium)
+                            .foregroundStyle(Color("PrimaryWhite"))
+                            .frame(width: 44, height: 44)
+                            .background(Color("PrimaryGreen"))
+                            .clipShape(.circle)
+                            .glassEffect()
+                            .frame(alignment: .trailing)
+                    }
+                    .sheet(isPresented: $isAddTransactionSheetPresented) {
+                        AddTransactionSheet()
+                    }
+                }
+                .frame(width: 360)
+                
+                MainCard()
+                
+                RecentActivitiesCard()
+                
+                Spacer()
+            }
+        }
     }
 }
 
