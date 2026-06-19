8a8e980b31b7f5c451664f2ef4b01db391f8d1b4
Muhammad Muthi' Nuritzan <muhammaditzan3@gmail.com>
2026-06-19 19:27:05 +0700

    Add: Add conten for SearchPage

--- Budgetin/View/SearchPage/SearchPage.swift
+++ Budgetin/View/SearchPage/SearchPage.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+@ViewBuilder
+func rowSearch(category: String) -> some View {
+    let style = getCategoryStyle(category: category)
+    
+    HStack{
+        HStack(spacing: 10) {
+            Image(systemName: style.icon)
+                .font(Font.subheadline)
+                .foregroundStyle(style.iconColor)
+                .frame(width: 35, height: 35)
+                .background(style.bgColor)
+                .clipShape(.circle)
+            
+            Text(category)
+                .foregroundStyle(Color("PrimaryBlack"))
+                .lineLimit(1)
+        }
+    }
+}
+
 struct SearchPage: View {
     @State private var searchText: String = ""
     
+    private let recommendations = [
+        "Meals",
+        "Household",
+        "Transportation",
+        "Shopping",
+        "Subscription",
+        "Health",
+        "Entertainment",
+        "Other"
+    ]
     
     private var filteredItems: [String] {
         if searchText.isEmpty {
-            return []
+            return recommendations
         }
-        return [].filter{$0.localizedCaseInsensitiveContains(searchText)}
+        return recommendations.filter{ $0.localizedCaseInsensitiveContains(searchText)}
     }
     
     var body: some View {
@@ -24,8 +54,8 @@ struct SearchPage: View {
                 Color("BackgroundColor")
                     .ignoresSafeArea()
                 
-                List(filteredItems, id: \.self){ item in
-                    Text(item)
+                List(filteredItems, id: \.self) { item in
+                    rowSearch(category: item)
                         .listRowBackground(Color.clear)
                 }
                 .navigationTitle("Search")
